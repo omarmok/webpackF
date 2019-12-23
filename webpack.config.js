05544fe262a659db28b5d7281294debc729778c8
Omar Mokhtar <al_fagomy22@hotmail.com>
2019-12-23 09:24:29 +0300

    up

--- webpack.config.js
+++ webpack.config.js
@@ -5,94 +5,108 @@ const webpack = require('webpack');
 const glob = require('glob');
 const PurifyCSSPlugin = require('purifycss-webpack');
 const isProd = process.env.NODE_ENV === 'production';
-const cssDev =['style-loader', 'css-loader','sass-loader'];
+const cssDev = ['style-loader', 'css-loader', 'sass-loader'];
 cssProde = ExtractTextPlugin.extract({
-  fallbackLoader: 'style-loader',
-  loader: ['css-loader?minimize=true','sass-loader',],
+  fallback: 'style-loader',
+
+  use: [{
+      loader: 'css-loader',
+      options: {
+
+        url: false,
+        minimize: true,
+        sourceMap: true
+      }
+    },
+    {
+      loader: 'sass-loader',
+      options: {
+        sourceMap: true
+      }
+    }
+  ],
+
   publicPath: './dist',
 
 });
-const cssConfig = isProd  ? cssProde :cssDev;
+const cssConfig = isProd ? cssProde : cssDev;
 
 const path = require("path");
 
 module.exports = {
 
-    entry: './src/js/app.js',
-    output: {
-        path: path.resolve(__dirname, "dist"),
-        filename: 'js/app.bundle.js',
-    },
-    module: {
-        rules: [
-            {
-                test: /\.scss$/,
-                use: cssConfig
-            },
-            {
-              test: /\.(gif|png|jpe?g|svg|)$/i,
-              use: [
-                // 'file-loader',
-                // 'file-loader?name=img/[name].[ext]',
-                // to short
-                'file-loader?name=[name].[ext]&outputPath=./img/&publicPath=./img',
-                //  to make img title same name
-                //  'file-loader?name[hash:6][name][ext]&outputPath=/img/',
-                // make img title  lenth
+  entry: './src/js/app.js',
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: 'js/app.bundle.js',
+  },
+  module: {
+    rules: [{
+        test: /\.scss$/,
+        use: cssConfig
+      },
+      {
+        test: /\.(gif|png|jpe?g|svg|)$/i,
+        use: [
+          // 'file-loader',
+          // 'file-loader?name=img/[name].[ext]',
+          // to short
+          'file-loader?name=[name].[ext]&outputPath=./img/&publicPath=./img',
+          //  to make img title same name
+          //  'file-loader?name[hash:6][name][ext]&outputPath=/img/',
+          // make img title  lenth
 
-                'image-webpack-loader',
-              ]
-          },
-
-
-          {
-            test: /\.(eot|svg|ttf|woff|woff2)$/,
-            use: [
-              'file-loader?name=[name].[ext]&outputPath=./fonts/&publicPath=/fonts/',
-            ]
-        }
+          'image-webpack-loader',
         ]
-    },
-    devServer: {
-        contentBase: path.join(__dirname, "dist"),
-        compress: true,
-        hot :true,
-        stats: "errors-only", // to see error in terminal
-        open: true
-         // to open new tab evry run
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: 'custom title',
-            minify: {
-                collapseWhitespace: true
-            },
-            hash: true,
-            template: './src/index.html',
-        }),
-
-        new HtmlWebpackPlugin({
-          title: 'about',
-          hash: true,
-          filename: 'about.html',
-          template: './src/about.html',
-      }),
+      },
 
 
-        new ExtractTextPlugin({
-           filename: './css/app.[hash:7].css',
-           disable: !isProd,
-           allChunks: true,
-
-
-
-        }),
-        new webpack.HotModuleReplacementPlugin(),
-        new PurifyCSSPlugin({
-          paths: glob.sync(path.join(__dirname, 'src/*.html')),
-          minimize:true
-        })
+      {
+        test: /\.(eot|svg|ttf|woff|woff2)$/,
+        use: [
+          'file-loader?name=[name].[ext]&outputPath=./fonts/&publicPath=/fonts/',
+        ]
+      }
     ]
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    compress: true,
+    hot: true,
+    stats: "errors-only", // to see error in terminal
+    open: true
+    // to open new tab evry run
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'custom title',
+      minify: {
+        collapseWhitespace: true
+      },
+      hash: true,
+      template: './src/index.html',
+    }),
+
+    new HtmlWebpackPlugin({
+      title: 'about',
+      hash: true,
+      filename: 'about.html',
+      template: './src/about.html',
+    }),
+
+
+    new ExtractTextPlugin({
+      filename: './css/app.[hash:7].css',
+      disable: !isProd,
+      allChunks: true,
+
+
+
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+    new PurifyCSSPlugin({
+      paths: glob.sync(path.join(__dirname, 'src/*.html')),
+      minimize: true
+    })
+  ]
 };
-
-
